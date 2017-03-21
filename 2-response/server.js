b2c97b967b9897e637626172b282c37c0dc6af36
code-zhangrui <code.zhangrui@gmail.com>
2017-03-21 23:11:37 +0800

    给之前的nodejs入门代码做了些注释

--- 2-response/server.js
+++ 2-response/server.js
@@ -9,14 +9,14 @@ function start(route, handle) {
 
     request.setEncoding("utf8"); //接收数据的编码格式为UTF-8
 
-    request.addListener("data", function(postDataChunk) {
+    request.addListener("data", function(postDataChunk) {  //注册了“data”事件的监听器，用于收集[每次]接收到的新数据块，并将其赋值给postData 变量
       postData += postDataChunk;  //
       console.log("Received POST data chunk '"+
       postDataChunk + "'.");  //
     });
 
-    request.addListener("end", function() {
-      route(handle, pathname, response, postData);  //
+    request.addListener("end", function() {  //将请求路由的调用移到end事件处理程序中，以确保它只会当所有数据接收完毕后才触发，并且只触发一次
+      route(handle, pathname, response, postData);  //把POST数据传递给请求路由，因为这些数据，请求处理程序会用到
     });
 
   }
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # learn nodejs 
+
+### 顺序：index=>server=>router=>handle
+#### index中要处理路径，这样路由模块只需要一个判断函数（判断路径是否对应着一个函数），就很精简；server中除了启动HTTP模块，还要吸收数据，最后一起给handle；handle就比较简单，做好功能性的函数就行了
